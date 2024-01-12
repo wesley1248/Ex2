4f1445b16ecfd7d4615ec3211359a4c677e090f0
wesley1248 <wesleytms.es@gmail.com>
2024-01-12 20:35:45 -0300

    Up

---
+++ Ex2 Antecessor/Ex2 Antecessor/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Quantos numeros vai digitar?: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= quantidade; i++)
+{
+
+    Console.Write($"Digite o {i} numero: ");
+    int numeroInicial = int.Parse(Console.ReadLine());
+
+    int numero = numeroInicial - 1;
+
+    Console.WriteLine($"O antecessor de {numeroInicial} é: {numero}");
+}
+
+Console.ReadKey();
